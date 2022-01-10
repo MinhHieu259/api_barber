2a26fb86d515aab0edcae6836bf5a9cbe33bf5eb
MinhHieu259 <mhieu7252@gmail.com>
2022-01-10 10:56:04 +0700

    add cot

--- app/Http/Controllers/Api/NhanvienController.php
+++ app/Http/Controllers/Api/NhanvienController.php
@@ -6,15 +6,18 @@ use App\Http\Controllers\Controller;
 use App\Models\nhanvien;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use phpDocumentor\Reflection\Types\Null_;
 
 class NhanvienController extends Controller
 {
     public function getNhanVienBySalon($gio, $id_salon)
     {
         $nhanvien = DB::table('nhanviens')->where('gioBatDauLam','<', $gio)->where('gioNghiLam','>', $gio)->where('id_salon', $id_salon)->get();
+
         return response()->json([
             'success' => true,
             'nhanvien' => $nhanvien
         ]);
+       
     }
 }
