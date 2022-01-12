3f144cb6725e47e07f4570090791a5868c1c4e0e
MinhHieu259 <mhieu7252@gmail.com>
2022-01-12 08:42:13 +0700

    hieu add

--- app/Models/Salon.php
+++ app/Models/Salon.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
-class Salon extends Model
+class Salon extends Model implements AuthenticatableContract
 {
+    use HasFactory, Authenticatable;
     public function nhanvien()
     {
         return $this->hasMany(Nhanvien::class);
