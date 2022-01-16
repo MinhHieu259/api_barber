71a75f89fcfbbd10cc05d0baab748c97e7dd0c4b
MinhHieu259 <mhieu7252@gmail.com>
2022-01-16 13:13:53 +0700

    add latitude

--- database/migrations/2022_01_16_061210_insert_column_latitude.php
+++ database/migrations/2022_01_16_061210_insert_column_latitude.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class InsertTblSalon extends Migration
+class InsertColumnLatitude extends Migration
 {
     /**
      * Run the migrations.
@@ -14,13 +14,8 @@ class InsertTblSalon extends Migration
     public function up()
     {
         Schema::table('salons', function (Blueprint $table) {
-            $table->string('username')->unique()->default("");
-            $table->string('password')->default("");
-            $table->unsignedBigInteger("soChoNgoi")->default(1);
-            $table->unsignedBigInteger("soNamThanhLap")->default(1);
-            
-
-                
+            $table->double("latitude")->default(16.068);
+            $table->double("longitude")->default(108.212);
         });
     }
 
@@ -32,8 +27,8 @@ class InsertTblSalon extends Migration
     public function down()
     {
         Schema::table('salons', function (Blueprint $table) {
-            $table->dropColumn("soChoNgoi");
-            $table->dropColumn("soNamThanhLap");
+            $table->dropColumn("latitude");
+            $table->dropColumn("longitude");
         });
     }
 }
